4f4026ff3de9afab901da1bdc108eab477e7b70f
Shalom code <chimdikeshalom@gmail.com>
2025-11-12 07:48:27 +0100

    First python Assignment

---
+++ demo.py
@@ -0,0 +1,196 @@
+# # #name = "Hello Shalom"
+# # #print(name)
+# # #DEMOPprint('Hello World')
+
+# name = "Shalom"
+# age= 19
+# print(f"My name is {name}.\nI am {age} years old.")
+# 
+# name = ['Shalom', 'Chimdike', 'Favour', 'Grace']
+# w, x, y, z = name 
+# print(w, x, y, z)
+
+# print("My name is Shalom.\nI'm a software developer.\nI'm 23 years old.")
+# x = "God "
+# y = "is "
+# z = "Good "
+# w = "All  the time"
+# print(x, y, z, w)
+# x = "awesome"
+
+# def myfunc():
+#     print("Python is " + x)
+
+# myfunc()  
+
+# x = "God"
+
+# def myGod():
+#     print("My " + x + " Is good")
+
+# myGod()  
+
+# x = "awesome"
+
+# def myfunc():
+#     x = "fantastic"
+#     print("python is " + x)
+# myfunc()  
+
+# # print("Python is " + x)
+# print("He is called 'Johnny'")
+# print('He is called "johnny"')
+
+# a = "Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."
+# print(a)
+
+# Strings are arrays
+# a = "Shalom"
+# print(a[5])
+
+# # Looping through a string
+# for x in "banana":
+#     print(x)
+# # String length
+# # The len() function returns the length of a string:
+# a = "Hello world"
+# print(len(a))
+# a = ['Mango', 'Orange', 'Guava']
+# print(len(a))
+# print(len(a[2]))
+
+#Check string
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+# msg = "Shalom Shoes your go-to for quality handmade shoes"
+# print("Shoes" in msg)
+
+# print("Shoes" in txt)
+# print("baxie" in msg)
+
+# String Creation
+s = 'Hello World'
+h = "Hello World"
+l = """Hello World"""
+print(s, h, l)
+
+# Assigning a variable
+a = "Python is fun to learn"
+print(a)
+
+
+# # Multiline String
+c = "Python is Powerful.\nIt is easy to learn.\nIt is loved by developers"
+print(c)
+
+# //String as Arrays
+
+b = "PYHON"
+print(b[0])
+print(b[2])
+print(b[4])
+
+# Looping Through a String
+for x in "Python":
+    print(x)
+for m in "Shalom":
+    print(m)    
+
+# String Length and Checking
+fruit = "banana"
+print(len(fruit))
+
+word = "Learning Python is cool."
+if "Python" in word:
+    print("Yes, 'Python' is in word.")
+
+if "Java" not in word:
+    print("'Java' is not found in the sentence.")
+
+
+name = "Ada"
+age = 18
+school = "Bright Future Academy"
+print(f"My name is {name}, I am {age} years old, and i attend {school}")
+
+country = "Nigeria"
+capital = "Abuja"
+print(f"The capital of {country} is {capital}.")
+
+first_name = "John "
+middle_name = "Paul "
+last_name = "Okoro"
+print(first_name, middle_name, last_name)
+
+food = "Rice"
+drink = "Juice"
+print(f"I love eating {food} with {drink}.")
+
+
+# Counting how many times "n" appears
+message = "Coding is fun"
+count = 0
+for char in message:
+    if char == "n":
+        count += 1
+print(count)    
+
+poem = """Python makes coding simple, Python makes me solve problems fast, Python is the language i enjoy learning."""
+print(poem.upper())
+shoes = "Shalom Shoes expertly crafts ladies footwears for elegance and comfort"
+
+count = 0
+for char in shoes:
+    if char == "n":
+        count += 1
+print(count)
+
+# name = " Hello World"
+# print(name[6:])
+# print(name.upper())
+# print(name.lower())
+# print(name.strip())
+# print(name.replace("H", "J"))
+# print(name.split(","))
+
+# # String CONcatenation
+
+# name = "Shalom "
+# age = '28 '
+# career = "Software Developer "
+# nationality = "Nigeria."
+# bio = name + age + career + nationality
+# print(bio)
+
+# price = 30000
+# print(f"I need {price: .2f} Dollars")
+# print(f"I need {price * 20} Dollars")
+
+# Escape paragraphs
+
+# txt = "They are the s0-called \"Vikings\" from the north."
+# b = "They are the s0-called \'Vikings\' from the north."
+# c = "They are the s0-called \\Vikings\\ from the north."
+# d = "They are the s0-called \nfrom the north."
+# e = "They are the s0-called \rVikings from the north."
+# f = "They are the s0-called \tVikings\t from the north."
+# g = "They are the s0-called \bVikings \bfrom the north."
+# h = "They are the s0-called \fVikings\f from the north."
+i = "\150\145\154\154\157"
+
+# variables = (txt, b, c, d, e, f, g, h, i)
+# for x in variables:
+#     print(x) 
+print(i)
+
+x = "God help me"
+print(type(x))
+
+
+
+
+
